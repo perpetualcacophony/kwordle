57703f2411ef7210389b467038479243fe3b2087
kate <142743629+perpetualcacophony@users.noreply.github.com>
2024-07-02 16:02:32 -0400

    manual impls for Letter

--- src/word/mod.rs
+++ src/word/mod.rs
@@ -26,6 +26,7 @@ pub use list::WordsList;
 
 /// Represents a single valid word from a specific [`WordsList`].
 #[derive(Debug, Clone, Copy, Hash, PartialEq, Eq, PartialOrd, Ord)]
+#[cfg_attr(feature = "serde_derive", derive(serde::Serialize, serde::Deserialize))]
 pub struct Word<const LEN: usize> {
     letters: Letters<LEN>,
 }
