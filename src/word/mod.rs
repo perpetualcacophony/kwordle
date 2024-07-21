4aee59072bddf82f4387741e10cbf811147b51e1
kate <142743629+perpetualcacophony@users.noreply.github.com>
2024-07-21 11:05:33 -0500

    added function to serialize as string

--- src/word/mod.rs
+++ src/word/mod.rs
@@ -41,6 +41,14 @@ impl<const LEN: usize> Word<LEN> {
         Ok(Self::new_unchecked(letters))
     }
 
+    #[cfg(feature = "serde")]
+    pub fn serialize_as_str<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        serializer.serialize_str(&self.to_string())
+    }
+
     /// Constructs a new `Word` from a [`Letters`] object if it exists in the given [`WordsList`].
     ///
     /// # Errors
